215d98330cddd1b9ffa6b88dd41a2edba993f632
KCornelius <KCornelius@W10KCORNT44P.Corp.Elmae>
2018-05-27 09:14:42 -0700

    Check in

--- HelloAgain/src/HelloAgain.java
+++ HelloAgain/src/HelloAgain.java
@@ -2,7 +2,8 @@
 public class HelloAgain {
 
 	public static void main(String[] args) {
-		System.out.println("Finally this works");
+		System.out.println("Finally this work");
+		System.out.println("After git");
 
 	}
 

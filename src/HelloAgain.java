241c3bbe394c0d70abcd333bb7f910972ea29e02
Kenneth Cornelius <kenneth.cornelius@gmail.com>
2018-05-27 13:52:09 -0700

    Test

--- src/HelloAgain.java
+++ src/HelloAgain.java
@@ -1,10 +1,12 @@
 
 public class HelloAgain {
 	
-	
+	final double a = 2.0;
 	// Add some comments
 	public static void main(String[] args) {
 		System.out.println("Alright, here's the last commit exa");
 	}
+	
+	
 
 }
